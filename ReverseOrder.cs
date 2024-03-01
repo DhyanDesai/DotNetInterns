d4cc140e4b2929f2697bd13d608f71235135e15e
Dhyan Desai <dhyan.desai@1Rintern.local>
2024-03-01 10:24:48 +0530

    files added to branch

--- ReverseOrder.cs
+++ ReverseOrder.cs
@@ -1,19 +1,25 @@
-using System;
 namespace DotNetInterns
 {
     class RevOrder{
-        public static void revchar(){
+        public void revchar(){
             /* that takes three letters and displays them in reverse order.*/
 
 
             string inputStr = "";
-            for (int i = 0;i<3;i++){
-                Console.WriteLine("Enter a character");
-                inputStr += Convert.ToChar(Console.ReadLine());
+            try
+            {
+                for (int i = 0;i<3;i++){
+                    Console.WriteLine("Enter a character");
+                    inputStr += Convert.ToChar(Console.ReadLine());
+                }
+                for(int i=inputStr.Length-1;i >=0;i--){
+                    Console.Write(inputStr[i]+" ");
+                } 
             }
-            for(int i=inputStr.Length-1;i >=0;i--){
-                Console.Write(inputStr[i]+" ");
-            } 
+            catch (NullReferenceException fe)
+            {
+               throw new Exception(fe.Message);
+            }
         }
     }
 }
