826f215bed6d95394a95d089acc422d0d34589b8
koyatina <twinkle.rana2612@gmail.com>
2024-03-04 22:57:58 +0530

    Anonymous method program

---
+++ AnonymousMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace DelegateDemo
+{
+    public class AnonymousMethods
+    {
+        public delegate string GreetingsDelegate(string name, int n);
+
+        static void Main(string[] args)
+        {
+            string Message = "Welcome to Bangtan World";
+
+            GreetingsDelegate gd = delegate (string name, int n)
+            {
+                return "Hello @" + name + " " + Message +" " + n;
+            };
+
+            string GreetingsMessage = gd.Invoke("tina", 7);
+            Console.WriteLine(GreetingsMessage);
+            Console.ReadKey();
+        }
+    }
+}
