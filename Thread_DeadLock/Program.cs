899cd980cc53eec377673731a4a8978ab437ee66
koyatina <twinkle.rana2612@gmail.com>
2024-03-07 23:24:49 +0530

    DeadLock Programs

---
+++ Thread_DeadLock/Program.cs
@@ -0,0 +1,61 @@
+﻿//deadlock situation occur
+//A deadlock in C# occurs when two or more threads are blocked forever, each waiting for the other to release a lock. 
+//Here's a simple example of a deadlock and a potential solution using the Monitor class to avoid deadlocks:
+
+
+using System;
+using System.Threading;
+
+class DeadlockExample
+{
+    static object lock1 = new object();
+    static object lock2 = new object();
+
+    static void Main()
+    {
+        Thread thread1 = new Thread(Method1);
+        Thread thread2 = new Thread(Method2);
+
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
+
+        Console.WriteLine("Execution completed.");
+    }
+
+    static void Method1()
+    {
+        lock (lock1)
+        {
+            Console.WriteLine("Method1 acquired lock1");
+
+            // Introducing a delay to make the deadlock more likely
+            Thread.Sleep(100);
+
+            lock (lock2)
+            {
+                Console.WriteLine("Method1 acquired lock2");
+            }
+        }
+    }
+
+    static void Method2()
+    {
+        lock (lock2)
+        {
+            Console.WriteLine("Method2 acquired lock2");
+
+            // Introducing a delay to make the deadlock more likely
+            Thread.Sleep(100);
+
+            lock (lock1)
+            {
+                Console.WriteLine("Method2 acquired lock1");
+            }
+        }
+    }
+}
+
+
---
+++ Thread_DeadLock_Moniter/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading;
+
+class MonitorTryEnterExample
+{
+    static object lockObject = new object();
+
+    static void Main()
+    {
+        Thread thread1 = new Thread(Method1);
+        Thread thread2 = new Thread(Method2);
+
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
+
+        Console.WriteLine("Execution completed.");
+    }
+
+    static void Method1()
+    {
+        // Attempt to acquire the lock
+        if (Monitor.TryEnter(lockObject))
+        {
+            try
+            {
+                Console.WriteLine("Method1 acquired the lock");
+
+                // Perform some work while holding the lock
+                Thread.Sleep(1000);
+            }
+            finally
+            {
+                Monitor.Exit(lockObject);
+                Console.WriteLine("Method1 released the lock");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Method1 couldn't acquire the lock immediately");
+        }
+    }
+
+    static void Method2()
+    {
+        // Attempt to acquire the lock
+        if (Monitor.TryEnter(lockObject))
+        {
+            try
+            {
+                Console.WriteLine("Method2 acquired the lock");
+
+                // Perform some work while holding the lock
+                Thread.Sleep(1000);
+            }
+            finally
+            {
+                Monitor.Exit(lockObject);
+                Console.WriteLine("Method2 released the lock");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Method2 couldn't acquire the lock immediately");
+        }
+    }
+}
+
