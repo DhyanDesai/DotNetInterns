3624dc77e3aaae84b817f90fd7e28654f6b16fce
koyatina <twinkle.rana2612@gmail.com>
2024-03-09 22:25:42 +0530

    thread programs

---
+++ Thread_2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading
+{
+    class Threading_1
+    {
+        public static void Main(string []args)
+        {
+            Console.WriteLine("main thread...");
+            Thread t1 = new Thread(Method1)
+            {
+                Name="Thread_1"
+            };
+            Thread t2 = new Thread(Method2)
+            {
+                Name="Thread_2"
+            };
+            Thread t3 = new Thread(Method3)
+            {
+                Name="Thread_3"
+            };
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            Console.WriteLine("Main Thread Ended");
+        }
+
+        static void Method1()
+        {
+            Console.WriteLine("Method1 started using:" + Thread.CurrentThread.Name);
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Method1 :" + i);
+            }
+            Console.WriteLine("Method1 Ended using " + Thread.CurrentThread.Name);
+        }
+
+        static void Method2()
+        {
+            Console.WriteLine("Method2 Started using " + Thread.CurrentThread.Name);
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Method2 :" + i);
+                if (i == 3)
+                {
+                    Console.WriteLine("Performing the Database Operation Started");
+                    //Sleep for 10 seconds
+                    Thread.Sleep(10000);
+                    Console.WriteLine("Performing the Database Operation Completed");
+                }
+            }
+            Console.WriteLine("Method2 Ended using " + Thread.CurrentThread.Name);
+        }
+        static void Method3()
+        {
+            Console.WriteLine("Method3 Started using " + Thread.CurrentThread.Name);
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Method3 :" + i);
+            }
+            Console.WriteLine("Method3 Ended using " + Thread.CurrentThread.Name);
+        }
+    }
+}
---
+++ Thread_Lock/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace Treading_lock
+{
+    class Thread_Lock
+    {
+        static object lockObject =new object();
+        public static void Main(string[]args)
+        {
+            Thread t1 = new Thread(SomeMethod)
+            {
+                Name="Thread1"
+            };
+            Thread t2 = new Thread(SomeMethod)
+            {
+                Name="Thread2"
+            };
+            Thread t3 = new Thread(SomeMethod)
+            {
+                Name="Thread3"
+            };
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            
+        }
+        public static void SomeMethod()
+        {
+            lock(lockObject)
+            {
+                Console.Write("[Welcome To The ");
+                Thread.Sleep(1000);
+                Console.WriteLine("World of Dotnet!]");
+            }
+        }
+    }
+}
