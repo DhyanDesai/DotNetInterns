fe0a67e3833c9213585f2ade75f54267b240afd1
Nivedita Tewani <nivedita.tewani@1Rintern.local>
2024-03-13 11:41:46 +0530

    constructor program

---
+++ Constructor_demo2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace ConstructorDemo
+{
+    class Constructor1
+    {
+        public int sum;
+        public Constructor1(int i, int j)
+        {
+            sum = i + j;
+             
+        }
+        public Constructor1(Constructor1 cons, int k)
+        {
+
+            sum = cons.sum + k;
+        }
+    }
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Constructor1 cons = new Constructor1(10, 20);
+            Constructor1 cons2 = new Constructor1(cons,30);
+            System.Console.WriteLine($"sum :{cons.sum}");   
+            System.Console.WriteLine($"sum :{cons2.sum}"); 
+            System.Console.WriteLine("Constructor done...");
+        }
+    }
+    
+}
+
